9116ba076f3029cd1b78f351557b457b78b89bf2
Arlen22 <arlenbee@gmail.com>
2017-10-06 23:27:39 -0400

    more work on this, no tsc yet

--- twcloud.ts
+++ twcloud.ts
@@ -15,19 +15,13 @@ declare const blobData: {
 // type rxjs = typeof Rx;
 type Hashmap<T> = { [K: string]: T };
 namespace wrapper {
-
+	const SESSION_KEY = 'twcloud-dropbox-session';
+	const ORIGINAL_KEY = 'twcloud-dropbox-original';
 	const SCRIPT_CACHE = "201710052";
 	//PARANOIA: Delete the window property of these libraries to prevent 
-	//Javascript in the page from messing with them.
-	// const Rx: rxjs = window.Rx;
-	// delete window.Rx;
-
-	// const Dropbox: typeof DropboxTypes.Dropbox = window.Dropbox;
-	// delete window.Dropbox;
-
-	//const $ = window.jQuery;
-	//delete window.$;
-	//delete window.jQuery;
+	//Javascript in the page from messing with them. We don't need to do
+	//this in the blob saver because nothing is loaded into the original
+	//page, and the blob page is completely contained in a function.
 
 	//declarations needed for the page
 	declare const $tw: any, store: any, clearMessage: Function, displayMessage: Function;
@@ -37,17 +31,31 @@ namespace wrapper {
 	// const { Observable, Subject, Subscriber, Subscription } = Rx;
 	type ListFolderResultEntries = DropboxTypes.files.ListFolderResult["entries"];
 
-	class twits {
-		targetOrigin: string = "*";
-		originalBlob: Blob;
-		messageSaverReady: boolean;
+	interface DropboxTiddlyWiki {
+		text: string,
+		blob: Blob,
+		metadata: DropboxTypes.files.FileMetadata,
+		path: string
+	}
+
+	interface SessionData {
+		token: {
+			access_token: string;
+			account_id: string;
+			token_type: "bearer";
+			uid: string;
+		};
+		type: string;
+		path: string;
+		metadata: DropboxTypes.files.FileMetadata;
+		profilepic: string;
+	}
+
+	class TwitsLoader {
 		iframe: HTMLIFrameElement;
 		user: DropboxTypes.users.FullAccount;
-		currentRev: string;
-		originalText: string;
-		originalPath: string;
 		client: Dropbox;
-		isProd: false
+		status: StatusHandler;
 		apiKeyFull = "gy3j4gsa191p31x"
 		apiKeyApps = "tu8jc7jsdeg55ta"
 		token: {
@@ -60,14 +68,14 @@ namespace wrapper {
 		getKey() {
 			return (this.type === "full" ? this.apiKeyFull : (this.type === "apps" ? this.apiKeyApps : ""))
 		}
-		constructor(public type: string, public preload: string) {
+		constructor(public type: string, preload: string) {
 			if (type !== "apps" && type !== "full") throw "type must be either apps or full"
 			this.client = new Dropbox({
 				clientId: this.getKey()
 			});
 
 			// Authenticate against Dropbox
-			this.setStatusMessage("Authenticating with Dropbox...");
+			this.status.setStatusMessage("Authenticating with Dropbox...");
 
 			if (document.location.hash) {
 				const data = document.location.hash.slice(1);
@@ -94,7 +102,7 @@ namespace wrapper {
 
 		// Main application
 		initApp(preload: string) {
-			this.clearStatusMessage();
+			this.status.clearStatusMessage();
 			this.client.usersGetCurrentAccount(undefined).then(res => {
 				this.user = res;
 				const profile = document.getElementById("twits-profile");
@@ -235,14 +243,15 @@ namespace wrapper {
 			// Read the TiddlyWiki file
 			// We can't trust Dropbox to have detected that the file is UTF8, 
 			// so we load it in binary and manually decode it
-			this.setStatusMessage("Reading HTML file...");
+			this.status.setStatusMessage("Reading HTML file...");
 			this.client.filesDownload({
 				path: path
 			}).then(res => {
 				//debugger;
-				this.currentRev = res.rev;
-				return new Promise<{ data: string, blob: Blob }>(resolve => {
+
+				return new Promise<DropboxTiddlyWiki>(resolve => {
 					const data: Blob = res.fileBlob;
+					delete res.fileBlob;
 					console.log(data.type);
 					var reader = new FileReader();
 					reader.addEventListener("loadend", () => {
@@ -251,16 +260,13 @@ namespace wrapper {
 						// however I think this is converting from UTF8 to UTF16  -Arlen ]
 						const byteData = new Uint8Array(reader.result);
 						const unicode = this.manualConvertUTF8ToUnicode(byteData);
-						this.originalPath = path;
-						this.originalText = unicode;
-						this.originalBlob = res.fileBlob;
-						resolve({ data: unicode, blob: res.fileBlob });
+						resolve({ text: unicode, blob: data, metadata: res, path });
 					});
 					reader.readAsArrayBuffer(data);
 					//debugger;
 				})
-			}).then(({ data, blob }) => {
-				this.loadTiddlywiki(data, blob)
+			}).then((data) => {
+				this.loadTiddlywiki(data)
 			})
 
 		}
@@ -289,7 +295,88 @@ namespace wrapper {
 		}
 
 
-		getStatusPanel() {
+
+
+		requestScript(url: string, type: "blob"): Promise<Blob>;
+		requestScript(url: string, type: "text"): Promise<string>;
+		requestScript(url: string, type: "arraybuffer"): Promise<ArrayBuffer>;
+		requestScript(url: string, type: XMLHttpRequestResponseType): any {
+			return new Promise<Blob>(resolve => {
+				var oReq = new XMLHttpRequest();
+				oReq.open("GET", url, true);
+				oReq.responseType = type;
+				oReq.onload = function (oEvent) {
+					//var blob = new Blob([oReq.response], {type: "image/png"});
+					resolve(oReq.response as Blob)
+				};
+				oReq.send();
+			})
+		}
+		buildNewBlob(data: DropboxTiddlyWiki) {
+			// const { blob, metadata, path, text } = data;
+			const startIndex = data.text.indexOf('<html');
+			const htmlheader = (startIndex === -1) ? "<" + "!DOCTYPE html>" : data.text.slice(0, startIndex);
+			const modHeader = '(function(module){ var exports = module.exports;';
+			const modFooter = '; return module.exports; })({ exports: {} });';
+
+			const scriptparts: (string | Blob | ArrayBuffer)[] = [
+				'(function(){ \n',
+			]
+			this.requestScript('dropbox.min.js', "blob").then(res => {
+				scriptparts.push("const Dropbox = (function(module){ var exports = module.exports; \n");
+				scriptparts.push(res);
+				scriptparts.push('\n; return module.exports; })({ exports: {} }); \n');
+				return this.requestScript('twcloud.js?' + SCRIPT_CACHE, "blob")
+			}).then(res => {
+				scriptparts.push(res);
+				scriptparts.push('\n })(); \n');
+				return this.requestScript('tiddly-saver-inject.js?' + SCRIPT_CACHE, "blob");
+			}).then(res => {
+				scriptparts.push(res);
+				const scriptblob = new Blob(scriptparts, { type: 'application/javascript' });
+
+				const htmlparts = [
+					htmlheader,
+					'\n<' + 'script src="' + URL.createObjectURL(scriptblob) + '"></' + 'script>\n',
+					data.blob
+				];
+
+				const blob = new Blob(htmlparts, { type: 'text/html' });
+				const url = URL.createObjectURL(blob);
+
+				const session: SessionData = {
+					token: this.token,
+					type: this.type,
+					path: data.path,
+					metadata: data.metadata,
+					profilepic: this.user.profile_photo_url
+				};
+
+				sessionStorage.setItem(SESSION_KEY, JSON.stringify(session));
+				sessionStorage.setItem(ORIGINAL_KEY, data.text);
+				location.href = url;
+			})
+		}
+
+		loadTiddlywiki(data: DropboxTiddlyWiki) {
+			const self = this;
+			//allow-same-origin 
+			document.body.innerHTML = `<iframe id="twits-iframe" sandbox="allow-same-origin allow-forms allow-modals allow-pointer-lock allow-popups allow-popups-to-escape-sandbox allow-scripts"></iframe>`;
+			this.iframe = document.getElementById('twits-iframe') as HTMLIFrameElement;
+			this.buildNewBlob(data);
+			return true;
+		}
+
+
+
+	}
+	class StatusHandler {
+
+		constructor(private profilepic: string) {
+			// this.getStatusPanel();
+		}
+
+		private getStatusPanel() {
 			// debugger;
 			var getElement = function (id, parentNode) {
 				parentNode = parentNode || document;
@@ -305,14 +392,14 @@ namespace wrapper {
 				message = getElement("twits-message", status),
 				progress = getElement("twits-progress", status);
 			status.style.display = "block";
-			if (this.user) {
+			if (this.profilepic) {
 				const profile = document.createElement('img');
-				profile.src = this.user.profile_photo_url;
+				profile.src = this.profilepic;
 				profile.classList.add("profile-pic");
 				status.insertBefore(profile, message);
 			}
 			return { status: status, message: message, progress: progress };
-		};
+		}
 
 		clearStatusMessage() {
 			var status = this.getStatusPanel();
@@ -325,89 +412,47 @@ namespace wrapper {
 				status.message.removeChild(status.message.firstChild);
 			}
 			status.message.appendChild(document.createTextNode(text));
+		}
+
+	}
+	class SaverHandler {
+		type: "full" | "apps";
+		status: StatusHandler;
+		originalPath: string;
+		originalText: string;
+		currentRev: string;
+		metadata: DropboxTypes.files.FileMetadata;
+		client: Dropbox;
+
+		get apiKeyFull() { return "gy3j4gsa191p31x" }
+		get apiKeyApps() { return "tu8jc7jsdeg55ta" }
+
+		token: {
+			access_token: string,
+			account_id: string,
+			token_type: "bearer",
+			uid: string
 		};
 
-		setProgress(text) {
-			var status = this.getStatusPanel();
-			while (status.progress.hasChildNodes()) {
-				status.progress.removeChild(status.progress.firstChild);
-			}
-			status.progress.appendChild(document.createTextNode(text));
-		};
+		getKey() {
+			return (this.type === "full" ? this.apiKeyFull : (this.type === "apps" ? this.apiKeyApps : ""))
+		}
 
-		// Display an error
-		showError(error) {
-			this.setStatusMessage("Error: " + error);
-			this.setProgress("");
-		};
-
-		requestScript(url: string) {
-			return new Promise<string>(resolve => {
-				var oReq = new XMLHttpRequest();
-				oReq.open("GET", url, true);
-				oReq.responseType = "text";
-
-				oReq.onload = function (oEvent) {
-					//var blob = new Blob([oReq.response], {type: "image/png"});
-					resolve(oReq.response as string)
-				};
-				oReq.send();
+		constructor(sessiondata: SessionData, original: string) {
+			this.token = sessiondata.token;
+			this.type = sessiondata.type as any;
+			this.currentRev = sessiondata.metadata.rev;
+			this.status = new StatusHandler(sessiondata.profilepic);
+			this.originalPath = sessiondata.path;
+			window.originalHTML = original;
+			this.metadata = sessiondata.metadata
+			this.client = new Dropbox({
+				clientId: this.getKey(),
+				accessToken: this.token.access_token
 			})
 		}
-		buildNewBlob(originalBlob, originalText) {
-			const startIndex = originalText.indexOf('<html');
-			const htmlheader = (startIndex === -1) ? "<" + "!DOCTYPE html>" : originalText.slice(0, startIndex);
-			const modHeader = '(function(module){ var exports = module.exports;';
-			const modFooter = '; return module.exports; })({ exports: {} });';
-			const lines = [
-				htmlheader,
-				'<' + 'script>(function(){',
-			]
-			this.requestScript('dropbox.min.js').then(res => {
-				lines.push("const Dropbox = " + modHeader + res + modFooter);
-				return this.requestScript('twcloud.js?' + SCRIPT_CACHE)
-			}).then(res => {
-				lines.push(res);
-				lines.push('})();');
-				return this.requestScript('tiddly-saver-inject.js?' + SCRIPT_CACHE)
-			}).then(res => {
-				lines.push(res);
-				lines.push('<' + '/script>')
-				// const trailer = '<' + 'script>if(typeof config !== "undefined" && config && config.options) config.options.chkHttpReadOnly = false;<' + '/script>';
-				var blob = new Blob([lines.join('\n\n'), originalBlob], { type: 'text/html' });
-				const url = URL.createObjectURL(blob);
-				delete this.client;
-				sessionStorage.setItem('twcloud-dropbox-twits', JSON.stringify(this));
-				location.href = url;
-			})
 
-
-
-		}
-
-		loadTiddlywiki(data: string, blob: Blob) {
-			const self = this;
-			//allow-same-origin 
-			document.body.innerHTML = `<iframe id="twits-iframe" sandbox="allow-same-origin allow-forms allow-modals allow-pointer-lock allow-popups allow-popups-to-escape-sandbox allow-scripts"></iframe>`;
-			this.iframe = document.getElementById('twits-iframe') as HTMLIFrameElement;
-			this.buildNewBlob(blob, data);
-			return true;
-			// const inject = `<script src="${
-			// 	location.origin + location.pathname.slice(0, location.pathname.lastIndexOf('/'))
-			// 	}/tiddly-saver-inject.js?${SCRIPT_CACHE}"><` + `/script>`;
-			// this.iframe.src = URL.createObjectURL(new Blob([blob, inject], { type: 'text/html' }));
-			// this.iframe.addEventListener('load', (ev) => {
-
-			// });
-			// window.addEventListener('message', this.onmessage.bind(this));
-			// return true;
-		}
 		setupSaving() {
-			// const handle = setInterval(() => {
-			// 	if (this.messageSaverReady) clearInterval(handle);
-			// 	else this.iframe.contentWindow.postMessage({ message: 'welcome-tiddly-saver' }, this.targetOrigin);
-			// }, 1000)
-			// window.addEventListener('message', this.onmessage.bind(this));
 			console.log('inserting message box');
 			var messageBox = document.getElementById("tiddlyfox-message-box");
 			if (!messageBox) {
@@ -423,7 +468,7 @@ namespace wrapper {
 					path = messageElement.getAttribute("data-tiddlyfox-path"),
 					content = messageElement.getAttribute("data-tiddlyfox-content"),
 					backupPath = messageElement.getAttribute("data-tiddlyfox-backup-path");
-					// messageId = "tiddlywiki-save-file-response-" + this.idGenerator++;
+				// messageId = "tiddlywiki-save-file-response-" + this.idGenerator++;
 
 				if (path !== document.location.toString()) return;
 
@@ -439,26 +484,24 @@ namespace wrapper {
 				messageElement.parentNode.removeChild(messageElement);
 			}, false);
 		}
-		onmessage(event) {
-			if (event.data.message === 'save-file-tiddly-saver') {
-				this.saveTiddlyWiki(event.data.data, (err) => {
-					event.source.postMessage({ message: 'file-saved-tiddly-saver', id: event.data.id, error: err }, this.targetOrigin);
-				})
-			}
-			else if (event.data.message === 'thankyou-tiddly-saver') {
-				this.messageSaverReady = true;
-				if (event.data.isTWC) event.source.postMessage({ message: 'original-html-tiddly-saver', originalText: this.originalText }, this.targetOrigin);
-			}
-			else if (event.data.message === 'update-tiddly-saver') {
-				if (event.data.TW5SaverAdded) {
-					alert("The saver for TW5 has now been added. Changes in TW5 will now be saved as usual.");
-				}
-			}
-
-		}
-
+		// onmessage(event) {
+		// 	if (event.data.message === 'save-file-tiddly-saver') {
+		// 		this.saveTiddlyWiki(event.data.data, (err) => {
+		// 			event.source.postMessage({ message: 'file-saved-tiddly-saver', id: event.data.id, error: err }, this.targetOrigin);
+		// 		})
+		// 	}
+		// 	else if (event.data.message === 'thankyou-tiddly-saver') {
+		// 		this.messageSaverReady = true;
+		// 		if (event.data.isTWC) event.source.postMessage({ message: 'original-html-tiddly-saver', originalText: this.originalText }, this.targetOrigin);
+		// 	}
+		// 	else if (event.data.message === 'update-tiddly-saver') {
+		// 		if (event.data.TW5SaverAdded) {
+		// 			alert("The saver for TW5 has now been added. Changes in TW5 will now be saved as usual.");
+		// 		}
+		// 	}
+		// }
 		saveTiddlyWiki(text, callback) {
-			this.setStatusMessage("Saving changes...");
+			this.status.setStatusMessage("Saving changes...");
 			//TODO: add progress tracker
 			this.client.filesUpload({
 				path: this.originalPath,
@@ -468,7 +511,7 @@ namespace wrapper {
 				},
 				contents: text
 			}).then(res => {
-				this.clearStatusMessage();
+				this.status.clearStatusMessage();
 				this.currentRev = res.rev;
 				callback(null);
 			}).catch(err => {
@@ -476,6 +519,7 @@ namespace wrapper {
 				callback(err.toString());
 			})
 		}
+
 	}
 	// only load if we are on http or https, otherwise we are probably in a blob
 	if (location.protocol.indexOf('http') === 0)
@@ -485,17 +529,16 @@ namespace wrapper {
 			const preload = url.searchParams.get('path');
 			if (!accessType) return;
 			document.getElementById('twits-selector').style.display = "none";
-			new twits(accessType, preload && decodeURIComponent(preload));
+			new TwitsLoader(accessType, preload && decodeURIComponent(preload));
 		}, false);
 	else if (location.protocol === "blob:")
 		document.addEventListener("DOMContentLoaded", function (event) {
-			const sessionstr = sessionStorage.getItem('twcloud-dropbox-twits');
-			if (!sessionstr) return alert('The session could not be loaded');
-			var session: twits = Object.assign(Object.create(twits.prototype), JSON.parse(sessionstr));
-			session.client = new Dropbox({
-				clientId: session.getKey(),
-				accessToken: session.token.access_token
-			});
-			session.setupSaving();
+			const sessionStr = sessionStorage.getItem(SESSION_KEY);
+			sessionStorage.setItem(SESSION_KEY, '');
+			const originalHTML = sessionStorage.getItem(ORIGINAL_KEY);
+			sessionStorage.setItem(ORIGINAL_KEY, '');
+			if (!sessionStr) return alert('The session could not be loaded');
+			const sessiondata = JSON.parse(sessionStr);
+			new SaverHandler(sessiondata, originalHTML);
 		}, false);
 }
